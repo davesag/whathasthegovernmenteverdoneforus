f192f9c508a8a991e025d6ec0d93890a6c4055d8
David Sag <davesag@gmail.com>
2013-06-15 18:35:26 +1000

    renamed, added more achievements

--- lib/helpers/navigation.rb
+++ lib/helpers/navigation.rb
@@ -4,7 +4,7 @@ module NavigationHelper
 
   # the navigation items in order.
   # if you add content you must add it to this array.
-  NAV_ITEMS = ['/', '/contact/']
+  NAV_ITEMS = ['/', '/romans/']
 
   def menu_item(text, target, attributes= {})
     # puts "#{text}, #{target}, #{attributes.inspect}"
