18930bc4f7f371a2c58798578e80c7ce29815eee
David Sag <davesag@gmail.com>
2013-06-15 16:18:35 +1000

    added more achievements

--- lib/helpers/achievements.rb
+++ lib/helpers/achievements.rb
@@ -7,7 +7,4 @@ module AchievementHelper
     @items.select { |i| i.identifier.start_with? '/achievement_details/' }
   end
 
-  def achievements_with_photos
-    @items.select { |i| i.identifier.start_with?('/achievement_details/') && i[:photo] != nil}
-  end
 end
